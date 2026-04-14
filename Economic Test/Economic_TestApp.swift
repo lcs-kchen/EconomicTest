d3e033288d2808b956ce8afa5d5c3b904043f07b
Kevin Chen <kchen@lcs.on.ca>
2026-04-14 09:00:08 -0400

    Initial Commit

---
+++ Economic Test/Economic_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Economic_TestApp.swift
+//  Economic Test
+//
+//  Created by Haowen Chen on 2026-04-14.
+//
+
+import SwiftUI
+
+@main
+struct Economic_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
